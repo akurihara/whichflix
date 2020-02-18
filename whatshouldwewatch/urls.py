5b5cac3d85e923f7bde40e8c32a732e540129f20
Alex Kurihara <alex.kurihara@gmail.com>
2020-02-18 00:07:12 -0500

    add API documentation

--- whatshouldwewatch/urls.py
+++ whatshouldwewatch/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import TemplateView
+from rest_framework.schemas import get_schema_view
 
 from whatshouldwewatch.elections.views import ElectionsView
 
@@ -8,4 +10,22 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     # Elections
     path("v1/elections", ElectionsView.as_view(), name="elections"),
+    # Open API schema
+    path(
+        "openapi",
+        get_schema_view(
+            title="WhatShouldWeWatch",
+            description="What Should We Watch",
+            version="1.0.0",
+        ),
+        name="openapi_schema",
+    ),
+    # ReDoc template for Open API schema
+    path(
+        "redoc/",
+        TemplateView.as_view(
+            template_name="redoc.html", extra_context={"schema_url": "openapi_schema"}
+        ),
+        name="redoc",
+    ),
 ]

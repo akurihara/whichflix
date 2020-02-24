82cd2de7bb64b15ee8c154a5012495db833d8224
Alex Kurihara <alex.kurihara@gmail.com>
2020-02-23 21:03:53 -0500

    configure for heroku

--- whatshouldwewatch/settings/base.py
+++ whatshouldwewatch/settings/base.py
@@ -21,7 +21,7 @@ from typing import List
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 DEBUG = True
 
---
+++ whatshouldwewatch/settings/production.py
@@ -0,0 +1,16 @@
+import dj_database_url
+import django_heroku
+
+from whatshouldwewatch.settings.base import *  # noqa
+
+#
+# django settings
+#
+
+ADMINS = [("Alex", "alex.kurihara@gmail.com")]
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
+
+db_from_environment = dj_database_url.config()
+DATABASES = {"default": db_from_environment}

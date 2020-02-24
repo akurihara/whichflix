4ecf81ca57480813f30b30b2ae4cbee37f88ed54
Alex Kurihara <alex.kurihara@gmail.com>
2020-02-23 21:26:44 -0500

    add device model to django admin

--- whatshouldwewatch/users/admin.py
+++ whatshouldwewatch/users/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from whatshouldwewatch.users.models import User
+from whatshouldwewatch.users.models import Device, User
 
+admin.site.register(Device)
 admin.site.register(User, UserAdmin)

9c84b9456c3997cf249ec3c4193359d3aac0b987
Alex Kurihara <alex.kurihara@gmail.com>
2021-02-15 16:08:17 -0500

    return none image url from search movie endpoint

--- whichflix/movies/builders.py
+++ whichflix/movies/builders.py
@@ -1,20 +1,28 @@
+from typing import Optional
+
 from whichflix.movies import constants
 from whichflix.movies.models import TMDBMovie
 
 
 def build_movie_document(tmdb_movie: TMDBMovie, tmdb_configuration: dict) -> dict:
-    image_url = "{base_url}{image_size}{poster_path}".format(
-        base_url=tmdb_configuration["images"]["secure_base_url"],
-        image_size=tmdb_configuration["images"]["poster_sizes"][4],
-        poster_path=tmdb_movie.poster_path,
-    )
     genres = [constants.GENRE_ID_TO_NAME[genre_id] for genre_id in tmdb_movie.genre_ids]
 
     return {
         "id": str(tmdb_movie._id),
         "title": tmdb_movie.title,
-        "image_url": image_url,
+        "image_url": build_image_url(tmdb_movie, tmdb_configuration),
         "description": tmdb_movie.overview,
         "release_year": tmdb_movie.release_date[:4],
         "genres": genres,
     }
+
+
+def build_image_url(tmdb_movie: TMDBMovie, tmdb_configuration: dict) -> Optional[str]:
+    if not tmdb_movie.poster_path:
+        return None
+
+    return "{base_url}{image_size}{poster_path}".format(
+        base_url=tmdb_configuration["images"]["secure_base_url"],
+        image_size=tmdb_configuration["images"]["poster_sizes"][4],
+        poster_path=tmdb_movie.poster_path,
+    )
